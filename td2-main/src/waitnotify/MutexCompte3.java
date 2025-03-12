34bb18ed37eaec861cc0c6e78c1b26b286808ae1
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-03-12 17:28:21 +0100

    Mutex

---
+++ td2-main/src/waitnotify/MutexCompte3.java
@@ -0,0 +1,56 @@
+package waitnotify;
+
+import java.util.stream.IntStream;
+
+public class MutexCompte3 {
+    public static void main(String[] args) {
+        final Compte compte = new Compte(100);
+
+        System.out.println("solde début : " + compte.consulter());
+        IntStream.range(0, 10000).mapToObj(i -> new Thread(() -> {
+            if (i%2 == 0)
+                compte.deposer(30);
+            else
+                compte.retirer(30);
+        })).forEach(t -> t.start());
+        System.out.println("solde fin : " + compte.consulter());
+
+    }
+
+    public static class Compte {
+        private int solde;
+        public Compte(int i) { solde = i; }
+        public synchronized void deposer(int montant) { solde = solde + montant; }
+        public synchronized void retirer(int montant) { solde = solde - montant; }
+        public int consulter() { return solde; }
+    }
+
+    public static class LivretA extends Compte {
+        private int solde;
+
+        public LivretA(int solde) {
+            super(solde);
+        }
+        public synchronized void deposer(int montant) {
+            if(solde > 1000) {
+                throw new IllegalArgumentException("solde trop haut");
+            }
+            super.deposer(montant);
+        }
+        public synchronized void retirer(int montant) {
+            if(solde < montant) {
+                throw new IllegalArgumentException("solde trop bas");
+            }
+            super.retirer(montant);
+        }
+    }
+
+}
+
+// Le solde de fin devrait être identique au solde de début, soit 100.
+// Or, il est variable (parfois positif parfois negatif, jamais le même) au fil des exécutions.
+// Cela est dû au fait que les threads ne sont pas exécutés dans le bon ordre.
+
+// Pour corriger, on ajoute synchronized
+
+// le thread.sleep ne doit jamais résoudre des pb de synchro
