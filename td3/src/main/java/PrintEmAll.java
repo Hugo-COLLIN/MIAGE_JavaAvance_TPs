506b54163aa05a090147162e095168c4e890b1e3
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-03-28 14:11:28 +0100

    print em all

--- td3/src/main/java/PrintEmAll.java
+++ td3/src/main/java/PrintEmAll.java
@@ -1,10 +1,12 @@
 import pokemon.PokemonRegistry;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class PrintEmAll {
-    public static void main(String[] args) throws URISyntaxException {
-        PokemonRegistry pr = new PokemonRegistry("http://localhost:8080/pokemons");
-        pr.fetch().forEach(System.out::println);
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        PokemonRegistry pr = new PokemonRegistry("https://www.data.gouv.fr/fr/datasets/r/04df345d-03dd-45f3-aed2-bf3dd55201a2");
+        //Afficher les 100 premiers
+        pr.fetch().stream().limit(100).forEach(System.out::println);
     }
 }
