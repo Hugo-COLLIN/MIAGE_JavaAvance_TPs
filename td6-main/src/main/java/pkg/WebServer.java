7c2c505f0924464ac27997e3abf8b04ca727e72e
Hugo COLLIN <hugo.collin1@outlook.fr>
2025-06-02 15:02:36 +0200

    unfinished TP6

--- td6-main/src/main/java/pkg/WebServer.java
+++ td6-main/src/main/java/pkg/WebServer.java
@@ -1,9 +1,20 @@
 package pkg;
 
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+
+import com.sun.net.httpserver.HttpServer;
+
+import pkg.handlers.HelloWorldHandler;
+
 public class WebServer {
 
     public static void main(String[] args) throws Exception {
-        // TODO ...
+        final HttpServer server = HttpServer.create(new InetSocketAddress(3000), 0);
+        server.createContext("/", new HelloWorldHandler());
+        // l'exécuteur par défaut exécute les requêtes dans le main thread
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
     }
 
 }
--- td6-main/src/main/java/pkg/handlers/HelloWorldHandler.java
+++ td6-main/src/main/java/pkg/handlers/HelloWorldHandler.java
@@ -1,6 +1,9 @@
 package pkg.handlers;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.util.Arrays;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -8,6 +11,23 @@ import com.sun.net.httpserver.HttpHandler;
 public class HelloWorldHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange t) throws IOException {
-        // TODO
+        try {
+            final String response = "Hello world :-)";
+            //final String response = "Hello world 🙂";
+            //final byte[] payload = response.getBytes(StandardCharsets.UTF_8);
+            t.getResponseHeaders().put("Content-Type", Arrays.asList("text/plain; utf-8"));
+            t.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length());
+            //t.sendResponseHeaders(200, payload.length);
+            if (!t.getRequestMethod().equals("HEAD")) {
+                try (final OutputStream os = t.getResponseBody()) {
+                    os.write(response.getBytes());
+                    //os.write(payload);
+                    os.close();
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace(System.err);
+            throw new IOException(e);
+        }
     }
 }
